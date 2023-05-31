ed49a97b2c73a2c37fcba85ac529d402c98fb976
AnaIranzo <aeiranzom@gmail.com>
2023-05-31 15:10:57 +0200

    add negative, flip filters

--- script.js
+++ script.js
@@ -34,6 +34,14 @@ Canvas.prototype = {
         if (tipo =='grises') {
             new Grises(this); 
         }
+
+        if (tipo =='negativo') {
+            new Negativo(this); 
+        }
+
+        if (tipo =='flip') {
+            new FlipHorizontal(this); 
+        }
     },
     limpiar: function(){
         var w = this.imagen.width;
@@ -90,6 +98,44 @@ function Grises(canvas){
         f.data[i]  = gris //rojo
         f.data[i+1] = gris // verde
         f.data [i+2] = gris //azul      
+    }
+    canvas.dibujar(f.imagenData)
+}
+
+function Negativo(canvas){
+    f = new Filtro(canvas);
+    for (let i = 0; i < f.data.length; i+=4) {// lo recorremos de 4 en 4 porque tenemos 4 canales (r,v,a, alfa)
+        //necesitamos el inverso
+        f.data[i]  = 255 - f.data[i]//rojo
+        f.data[i+1] = 255 - f.data[i+1]// verde
+        f.data [i+2] = 255 - f.data[i+2]//azul      
+    }
+    canvas.dibujar(f.imagenData)
+}
+
+function FlipHorizontal(canvas) {
+    //algoritmo cubeta
+    f = new Filtro(canvas);
+    for (let x = 0; x < f.w/2; x++) {
+        for (let y = 0; y < f.h; y++) {
+            //coordenadas
+            var i = (y*f.w+x); //de izq a derecha
+            var j = (y*f.w+(f.w-1)-x)// f -1 porque nos da el ultimo de los valores, abarcamos de derecha a izq       
+
+            //valores temporales para los tres canales
+            var p1 = f.data[i*4];
+            var p2 = f.data[i*4+1];
+            var p3 = f.data[i*4+2];
+
+            f.data[i*4] = f.data[j*4];
+            f.data[i*4+1] = f.data[j*4+1];
+            f.data[i*4+2] = f.data[j*4+2];
+
+            f.data[j*4] = p1;
+            f.data[j*4+1] = p2;
+            f.data[j*4+2] = p3;
+        }
+        
     }
     canvas.dibujar(f.imagenData)
 }
